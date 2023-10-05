f33490184fe6338bc2f8f0a0a61001b6adad11ae
elisa0871 <146981596+elisa0871@users.noreply.github.com>
2023-10-05 16:19:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,33 @@
 import streamlit as st
+import random
 
-# Titel der Webanwendung
-st.title('Meine Streamlit-Webanwendung')
+# Liste der Hintergrundbilder
+background_images = ["background1.jpg", "background2.jpg", "background3.jpg"]
 
-# Einfacher Text
-st.write('Willkommen auf meiner Website!')
+# Funktion zum Anzeigen eines zufälligen Hintergrundbilds
+def set_random_background():
+    background_image = random.choice(background_images)
+    st.markdown(
+        f'<style>body{{background-image: url("{background_image}"); background-size: cover;}}</style>',
+        unsafe_allow_html=True,
+    )
 
-# Fügen Sie eine Schaltfläche hinzu
-if st.button('Klick mich'):
-    st.write('Du hast die Schaltfläche geklickt!')
+# Liste der Bilder
+images = ["image_a.jpg", "image_b.jpg", "image_c.jpg", "image_d.jpg", "image_e.jpg"]
 
-# Fügen Sie eine Texteingabe hinzu
-user_input = st.text_input('Gib etwas ein:')
-st.write('Du hast eingegeben:', user_input)
+# Streamlit-Anwendung
+st.title("Bildwechsel-Webanwendung")
+
+# Anzeigen der Hintergrundbilder
+set_random_background()
+
+# Buttons für den Bildwechsel
+button_cols = st.beta_columns(5)
+for i in range(len(images)):
+    if button_cols[i].button(f"Button {i+1}"):
+        st.image(images[i], use_column_width=True)
+
+# Reset-Knopf
+if st.button("Alle Bilder zurücksetzen"):
+    set_random_background()
+
